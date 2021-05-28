bdb46c749dfe0fe91ba43be8d68afc8b065799e2
Guilherme Felipe Pereira de Melo <62664524+GuilhermeMelo-sys@users.noreply.github.com>
2021-05-28 13:02:11 -0300

    Implementando ListChats.vue.

--- src/components/chat/ListChats.vue
+++ src/components/chat/ListChats.vue
@@ -1,9 +1,22 @@
 <template>
-    <div> olá chat </div>
+    <div class="box">
+        <div class="item" v-for="chat in chats" :key="chat">
+            <span v-html="chat.nome" :data-teste="chat.nome" />
+            <span v-html="chat.id" :data-teste="chat.id" />
+        </div>
+    </div>
 </template>
 
 <script lang="ts">
     import { defineComponent } from "vue";
 
-    export default defineComponent({});
+    export default defineComponent({
+        name: "ListChats",
+        props: {
+            chats: null,
+        },
+        setup() {
+            return {};
+        },
+    });
 </script>
