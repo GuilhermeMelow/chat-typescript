5ec20a1cabf61cad6b663315a3f291f691c2c105
Guilherme Felipe Pereira de Melo <62664524+GuilhermeMelo-sys@users.noreply.github.com>
2021-05-28 15:16:46 -0300

    Limpando e ajustando aplicação.

--- src/components/login/functions/UseSobrenome.ts
+++
@@ -1,8 +0,0 @@
-import { IUser } from "@/types/IChatService";
-import { computed, Ref } from "vue";
-
-export default function useSobrenome(usuario: Ref<IUser>) {
-    const sobrenome = computed(() => usuario.value.nome + "123");
-
-    return { sobrenome };
-}
