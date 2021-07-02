fba623d41a51b2f1ff7d669966f3d93a3032f577
Guilherme Felipe Pereira de Melo <62664524+GuilhermeMelo-sys@users.noreply.github.com>
2021-07-02 18:12:14 -0300

    Retirando duplicação de chat da lista.

--- src/services/MemoryChat.ts
+++ src/services/MemoryChat.ts
@@ -4,14 +4,14 @@ export class InMemoryChat implements IChatService {
     private _chats: IChat[] = [];
 
     async adicionar(chat: IChat): Promise<number> {
+
         return new Promise<number>((resolve) => {
             resolve(this._chats.push(chat))
         });
     }
 
     async entrar(nome: string): Promise<IChat> {
-        if (nome === "")
-            throw new Error("Não é possível entrar com este nome.");
+        if (nome === "") throw new Error("Não é possível entrar com este nome.");
 
         const chat: IChat = { id: "sadf", nome: nome };
         await this.adicionar(chat);
@@ -20,6 +20,7 @@ export class InMemoryChat implements IChatService {
     }
 
     async pegarChats(): Promise<IChat[]> {
-        return new Promise<IChat[]>((resolve) => resolve(this._chats))
+        const copyChats: IChat[] = this._chats.map(c => ({ ...c }));
+        return new Promise<IChat[]>((resolve) => resolve(copyChats))
     }
 }
