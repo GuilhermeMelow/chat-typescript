3693b3ed7e13b967a57a80e8f24870e03bff822c
Guilherme Felipe Pereira de Melo <62664524+GuilhermeMelo-sys@users.noreply.github.com>
2021-07-02 18:08:40 -0300

    Criando componente de criação de chat.

---
+++ src/components/chat/CriarChat.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="criarChat">
+        <pre>{{ chats }}</pre>
+        <label for="nome"> Nome Chat: </label>
+        <input type="text" v-model="nomeChat" />
+
+        <button @click="adicionar(nomeChat)">Criar</button>
+    </div>
+</template>
+
+<script lang="ts">
+    import { defineComponent, ref } from "vue";
+    import useChats from "@/components/chat/functions/UseChats";
+
+    export default defineComponent({
+        async setup() {
+            const nomeChat = ref("");
+            const x = await useChats();
+
+            return { nomeChat, ...x };
+        },
+    });
+</script>
