066259c7667d54df42ccb50b5fbb0191cec22001
Guilherme Felipe Pereira de Melo <62664524+GuilhermeMelo-sys@users.noreply.github.com>
2021-07-20 09:43:38 -0300

    Alterando Enviador.

---
+++ src/components/Enviador.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="criarChat">
+        <label for="Data" v-text="label" />
+        <input type="text" id="Data" v-model="data" data-teste="nome" />
+        <button @click="send(data)" data-teste="criar" v-text="'Enviar'" />
+    </div>
+</template>
+
+<script lang="ts">
+    import { defineComponent } from "vue";
+
+    export default defineComponent({
+        name: "Criar",
+        props: {
+            label: String,
+        },
+        setup(props, { emit }) {
+            return { data: "", send: (data: string) => emit("send", data) };
+        },
+    });
+</script>
