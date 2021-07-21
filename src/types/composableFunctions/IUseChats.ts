816059d6f9f6ca59bf19d5531ac389de2d12f9fb
Guilherme Felipe Pereira de Melo <62664524+GuilhermeMelo-sys@users.noreply.github.com>
2021-07-21 14:44:22 -0300

    Criando diretorio para composables functions

--- src/types/composableFunctions/IUseChats.ts
+++ src/types/composableFunctions/IUseChats.ts
@@ -2,8 +2,6 @@ import { Chat } from "@/types/Chat";
 import { ComputedRef, Ref } from "vue";
 
 export interface IUseChats {
-    criarConversa(nome: string): void;
-    abrirConversa(id: string): void;
     chats: Ref<Chat[]>;
     chatsOpen: ComputedRef<Chat[]>;
 }
