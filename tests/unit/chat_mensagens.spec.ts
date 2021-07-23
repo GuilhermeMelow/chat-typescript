335f311119a616fc3090cb5fb7714754e7ee7485
Guilherme Felipe Pereira de Melo <62664524+GuilhermeMelo-sys@users.noreply.github.com>
2021-07-23 09:41:20 -0300

    Fix testes.

--- tests/unit/chat_mensagens.spec.ts
+++ tests/unit/chat_mensagens.spec.ts
@@ -4,14 +4,13 @@ function build() {
     return { chat, mensagem: "teste" };
 }
 
-test("Ao chamar adicionar, a mesma deve estar na lista de mensagens", () => {
+test("Ao enviar uma mensagem, a mesma deve estar na lista de mensagens", () => {
     // Arrange 
     const { chat, mensagem } = build();
 
     // Act
     chat.adicionarMensagem(mensagem);
-    const mensagens = chat.getMensagens();
-    const resultMessage = mensagens.find(p => p == mensagem);
+    const resultMessage = chat.mensagens.find(p => p == mensagem);
 
     // Assert
     const hasMessage = resultMessage != undefined;
