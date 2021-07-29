d1f3b0c1b384f76dd8b2012078c72d90cf6260c0
Guilherme Felipe Pereira de Melo <62664524+GuilhermeMelo-sys@users.noreply.github.com>
2021-07-29 11:37:38 -0300

    Indexando as funções.

--- src/components/chat/functions/Index.ts
+++ src/components/chat/functions/Index.ts
@@ -1,7 +1,4 @@
-import { UseAbrirConversa } from "./UseAbrirConversa";
-import { UseCriarConversa } from "./UseCriarConversa";
-import { UseMensagens } from "./UseMensagens";
 import { UseChatsOpen } from "./UseChatsOpen";
 import { UseChats } from "./UseChats";
 
-export { UseAbrirConversa, UseChats, UseCriarConversa, UseChatsOpen, UseMensagens };
+export { UseChats, UseChatsOpen };
