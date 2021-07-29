97824b59b5eda10d755fa25c6e33e32e9be083c0
Guilherme Felipe Pereira de Melo <62664524+GuilhermeMelo-sys@users.noreply.github.com>
2021-07-29 11:36:55 -0300

    Trazendo algumas functions para UseChats.

--- src/types/composableFunctions/IUseAbrirConversa.ts
+++
@@ -1,3 +0,0 @@
-export interface IUseAbrirConversa {
-    abrirConversa(id: string): void
-}
--- src/types/composableFunctions/IUseChats.ts
+++ src/types/composableFunctions/IUseChats.ts
@@ -3,4 +3,6 @@ import { Ref } from "vue";
 
 export interface IUseChats {
     chats: Ref<Chat[]>;
+    abrirConversa(id: string): void;
+    criarConversa(nome: string): void;
 }
