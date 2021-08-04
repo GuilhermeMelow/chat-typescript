dc8c3faf3fde96ba64d74cc69247ce19cb65f5ea
Guilherme Felipe Pereira de Melo <62664524+GuilhermeMelo-sys@users.noreply.github.com>
2021-08-04 09:41:20 -0300

    Retirando interface das composables functions.

--- src/types/composableFunctions/IUseChatsOpen.ts
+++
@@ -1,8 +0,0 @@
-import { Chat } from "@/types/Chat";
-import { ComputedRef, Ref } from "vue";
-
-export interface IUseChatsOpen {
-    chatsOpen: ComputedRef<Chat[]>;
-    activeChat: Ref<Chat | null>;
-    send(mensagem: string): void;
-}
