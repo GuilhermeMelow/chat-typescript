1715ef957a79c5fba5ef81d8104ddf7659252a49
Guilherme Felipe Pereira de Melo <62664524+GuilhermeMelo-sys@users.noreply.github.com>
2021-08-10 00:43:20 -0300

    Removendo chatSetup.

--- tests/unit/ChatSetup.ts
+++
@@ -1,7 +0,0 @@
-import { Chat } from "@/types/Chat";
-
-export const chats = [
-    new Chat("teste1", "1"), new Chat("teste2", "2"), new Chat("teste3", "3")
-];
-
-export const chat = new Chat("teste", "123");
