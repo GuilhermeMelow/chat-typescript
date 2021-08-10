cadac43239e50d95f5781544d0ee21043485605c
Guilherme Felipe Pereira de Melo <62664524+GuilhermeMelo-sys@users.noreply.github.com>
2021-08-10 10:28:51 -0300

    Limpando UseChats.

--- src/views/chat/functions/UseChats.ts
+++ src/views/chat/functions/UseChats.ts
@@ -1,6 +1,6 @@
 import { IStore } from "@/types/IStore";
 import { Chat } from "@/types/Chat";
-import { computed, onMounted } from "vue";
+import { computed } from "vue";
 
 export function UseChats(store: IStore) {
 
