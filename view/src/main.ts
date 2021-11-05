a93e2f48a6dfb1ab79a5afd48b8e38eb8ce51662
GuilhermeMelow <guilhermefepeme@hotmail.com>
2021-11-05 15:42:49 -0300

    Adicionando quasar.

--- view/src/main.ts
+++ view/src/main.ts
@@ -1,6 +1,14 @@
 import { createApp } from 'vue'
+import 'quasar/dist/quasar.css'
+import '@quasar/extras/material-icons/material-icons.css'
 import App from './App.vue'
 import './registerServiceWorker'
 import { router } from './router'
+import { Quasar } from 'quasar'
 
-createApp(App).use(router).mount('#app')
+createApp(App).use(Quasar, {
+    config: {},
+    brand: {
+        primary: '#333',
+    },
+}).use(router).mount('#app')
