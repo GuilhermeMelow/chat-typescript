dcfd6363091b34d1bd62bc9c6bd43f50f58900f0
GuilhermeMelow <guilhermefepeme@hotmail.com>
2021-11-08 11:10:16 -0300

    Criando propriedade para o nome do botão de envio.

--- view/src/components/Enviador.vue
+++ view/src/components/Enviador.vue
@@ -1,10 +1,10 @@
 <template>
     <div class="q-pb-md q-mx-auto">
-        <q-input style="width:200px" class=" q-ma-auto" label="NickName" v-model="text" data-teste="nome" />
+        <q-input style="width:200px" class=" q-ma-auto" :label="label" v-model="text" data-teste="nome" />
     </div>
-    <div class="q-ma-md" style="heigth: 100%">
+    <div class="q-ml-md">
         <q-btn size="md" class="my-button q-mx-auto" data-teste="criar" @click="send()">
-            <span class="my-font q-ma-sm"> Sign in </span>
+            <span class="my-font q-ma-sm"> {{ buttonTitle }}</span>
         </q-btn>
     </div>
 </template>
@@ -16,6 +16,7 @@
         name: "Criar",
         props: {
             label: String,
+            buttonTitle: String,
         },
         setup(props, { emit }) {
             const text = ref("");
@@ -32,8 +33,6 @@
 </script>
 
 <style lang="scss" >
-    @import url("https://fonts.googleapis.com/css2?family=Roboto:wght@500&display=swap");
-
     .my-font {
         font-family: "Roboto", sans-serif;
         font-size: 0.85rem;
